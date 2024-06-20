645f0651d235a1125adba33d058c2ed749ebc243
Sunil <sv784333@gmial.com>
2024-06-20 22:39:57 +0530

    done

--- my-project/src/pages/Home.jsx
+++ my-project/src/pages/Home.jsx
@@ -50,7 +50,7 @@ function Home() {
         <Link to="/addFlight" className=" bg-blue-500 text-white dark:text-black p-2 rounded-md ">Add new Flight</Link>
       </div>
        <div className="elative overflow-x-auto">
-         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">>
+         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
                 <th scope="col" className="px-6 py-3">Flight Name</th>
